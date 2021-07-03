882dfe24e487f738915d3ab56bddf4a2f03b2a67
Kristina Borisova Hadzhieva <kristinahadzhieva111@gmail.com>
2021-07-03 13:54:36 +0000

    Amend style on quiz.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -172,3 +172,41 @@ let incrementScore = num => {
 };
 
 startQuiz();
+
+
+// -- End Game Page Script -- //
+//  Variables 
+const username = document.querySelector('#username');
+const saveScore = document.querySelector('#save-high-score');
+const finalScore = document.querySelector('#game-final-score');
+const recentScore = localStorage.getItem('recentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+finalScore.innerText = mostRecentScore;
+// User needs to type their name into the input container to remove the disabled option on the save button. //
+username.addEventListener('keyup', () => {
+    saveScore.disabled = !username.value;
+});
+/*  When save button is clicked, save the user name and score. */
+let saveHighScore = e => {
+    e.preventDefault();
+
+    const score = {
+        score: recentScore,
+        name: username.value
+    };
+
+    highScores.push(score);
+
+    highScores.sort((a, b) => {
+        return b.score - a.score;
+    });
+
+    highScores.splice(6);
+
+    localStorage.setItem(`highScores`, JSON.stringify(highScores));
+    window.location.assign('https://Kristina30.io/british-culture-quiz/end-quiz.html');
+
+
+};
